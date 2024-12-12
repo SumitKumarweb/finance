8b874dfbecc49ec454e50f2895dd62b127e6df76
Sumit Kumar <bingostudypoint@gmail.com>
2024-12-13 01:00:39 +0530

    update merge app.jsx and apply routing

--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,12 @@ function App() {
 
   return (
      <>
-<<<<<<< HEAD
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/income' element={<Income/>} />
           <Route path='/transaction' element={<Transaction/>} />
           <Route path='/expense' element={<Expense/>} />
         </Routes>
-=======
-        <Auth/>
->>>>>>> db422c3c5b880617461c6e324bb46adaed00bccc
       </>
   )
 }
