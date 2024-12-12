13c54ffc2048a293bdb5dd1e3af748b3eabea084
Sumit Kumar <bingostudypoint@gmail.com>
2024-12-13 01:49:50 +0530

    resolve the error of firebase

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,16 @@ import './App.css'
 import Auth from "./Components/Auth/Auth"
 import Home from "./Components/Home/Home"
 import Income from "./Components/Income/Income"
-import Nav from "./Components/Nav/Nav"
-import ShowData from "./Components/ShowData/ShowData"
 import Expense from "./Components/Expense/Expense"
 import Transaction from "./Components/Transaction/Transaction"
 import { Route, Routes } from 'react-router-dom'
 
 
 function App() {
-
-
-
   return (
      <>
         <Routes>
-          <Route path='/' element={<Home/>} />
+          <Route path='/' element={<Auth/>} />
           <Route path='/income' element={<Income/>} />
           <Route path='/transaction' element={<Transaction/>} />
           <Route path='/expense' element={<Expense/>} />
