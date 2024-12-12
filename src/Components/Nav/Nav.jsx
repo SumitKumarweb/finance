12aaf59fe42af444f62334ce9b7742f388cddf3e
fw24-269 <tarunsaini3500@gmail.com>
2024-12-13 01:44:22 +0530

    navigation added

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,16 +1,21 @@
 import React  from 'react'
 import './nav.css'
 import log from './log.webp'
+import { useNavigate } from 'react-router-dom'
+
 function Nav() {
- 
+ let navigate = useNavigate();
+ function handleNavigate(path) {
+  navigate(path);
+ }
    return (
     <nav>
         <img src={log} alt="logo" />
         <div>
-            <button>Saving</button>
-            <button>Income</button>
-            <button>Expense</button>
-            <button>Transaction</button>
+            <button onClick={()=>handleNavigate('/home')}>Saving</button>
+            <button onClick={()=>handleNavigate('/income')}>Income</button>
+            <button onClick={()=>handleNavigate('/expense')}>Expense</button>
+            <button onClick={()=>handleNavigate('/transaction')}>Transaction</button>
         </div>
     </nav>
   )
---
+++ src/Components/Saving/savings.jsx
@@ -0,0 +1,9 @@
+
+function Savings() {
+
+    return (
+        <>
+        
+        </>
+    )
+}
